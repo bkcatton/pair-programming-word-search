433793aa2fcd80ee97d9d7421d49496fe738d1dc
bkcatton <ben.k.catton@gmail.com>
2022-01-20 20:09:51 -0800

    this will search for word on the horizontal and vertical, and backwards bothways.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,6 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
+
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -32,6 +33,21 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present and backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['N', 'G', 'I', 'S', 'P', 'O', 'T', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'STOPSIGN')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    
+        //loop though the same index of each row
+        // push to an empty the value of the index of each row
+        //check if the string we just made matches the given string
+        // return true
+        //otherwise return false
+    if (letters.length === 0) return false; 
+    let resultsArray = [];
+    for (let i = 0; i < letters.length; i++) {
+        console.log(letters[i]);
+        let rowReturn = letters[i].join("");
+        if (rowReturn === word) return true;
+        reverseHorizontal = rowReturn.split("").reverse().join("");
+        if (reverseHorizontal === word) return true;
+
+        for (let j = 0; j <= letters[i].length; j++) {
+           resultsArray.push(letters[j][i]);
+        }
+        let stringReturn = resultsArray.join("");
+        console.log(stringReturn);
+        if (stringReturn === word) return true;
+        //reverse string and check if equal to word
+        reverseArrayVertical = stringReturn.split("").reverse().join("");
+        console.log(reverseArrayVertical);  
+        if (reverseArrayVertical === word) return true;
+        resultsArray = [];
     }
+    return false;
 }
 
 module.exports = wordSearch
